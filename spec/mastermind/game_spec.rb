1bbe6759388a4f1efe24590501cb15c21fc669c4
Evan Tedesco <Evantedesco@gmail.com>
2014-08-03 20:27:42 -0600

    changed the rest of the game spec

--- spec/mastermind/game_spec.rb
+++ spec/mastermind/game_spec.rb
@@ -5,8 +5,8 @@ module Mastermind
     it 'should have a beginner code of 4 colors' do
       code = Game.new.code
 
-      code.should be_a String
-      code.length.should == 4
+      expect(code.is_a?(String)).to eq(true)
+      expect(code.length).to eq(4)
     end
 
     it 'should seem to randomly generate colors' do
@@ -33,38 +33,39 @@ module Mastermind
     describe 'guessing colors' do
       it 'should give the number of correct colors when there is one match' do
         game = Game.new('rgby')
-        game.correct_colors('rrrr').should == 1
+        expect(game.correct_colors('rrrr')).to eq(1)
       end
 
       it 'should give the number of correct colors when all match' do
         game = Game.new('rgby')
-        game.correct_colors('ybgr').should == 4
+        expect(game.correct_colors('ybgr')).to eq(4)
       end
 
       context 'when guess contains multiples' do
         it 'should only give one letter correct' do
           game = Game.new('rgby')
-          game.correct_colors('rrrr').should == 1
+
+          expect(game.correct_colors('rrrr')).to eq(1)
         end
       end
 
       context 'when code contains multiples' do
         it 'should only give one letter correct' do
           game = Game.new('rrrr')
-          game.correct_colors('rgby').should == 1
+          expect(game.correct_colors('rgby')).to eq(1)
         end
       end
 
       context 'when code and guess have multiple matches' do
         it 'should count them correctly' do
           game = Game.new('rrby')
-          game.correct_colors('rrrr').should == 2
+          expect(game.correct_colors('rrrr')).to eq(2)
         end
       end
 
       it 'should be case insensitive' do
         game = Game.new('rgby')
-        game.correct_colors('YBGR').should == 4
+        expect(game.correct_colors('YBGR')).to eq(4)
       end
     end
   end
